9cd21e223f1a5d590c95e263a1e7a5c38d106ab3
Youssouph Camara <88164361+YousCode@users.noreply.github.com>
2025-01-16 19:51:06 +0100

    delete all

--- src/constants/assets.ts
+++
@@ -1,8 +0,0 @@
-export const ASSETS = {
-    LOGO: "~/assets/logo.png",
-    SOCIAL: {
-        APPLE: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
-        FACEBOOK: "https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg",
-        GOOGLE: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg",
-    }
-};
--- src/styles/background.ts
+++
@@ -1,8 +0,0 @@
-export const gradientBackground = {
-  background: {
-    type: "linear",
-    colors: ["#FFFFFF", "#E6EEFF", "#EEE6FF", "#E6EEFF"],
-    stops: [0, 0.3, 0.7, 1],
-    angle: 135
-  }
-};
